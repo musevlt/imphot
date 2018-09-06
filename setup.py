aefe096adf4e54c1acd74b4d93e67e6fd44258f3
Simon Conseil <contact@saimon.org>
2018-09-06 23:36:28 +0200

    Reorganize files and add missing ones

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imphot',
+    version='0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['numpy', 'matplotlib', 'astropy', 'mpdaf', 'aplpy',
+                      'lineid_plot', 'joblib', 'six', 'lmfit', 'dataset',
+                      'scipy', 'adjustText', 'termcolor'],
+    scripts=[
+        'imphot/scripts/fit_photometry',
+        'imphot/scripts/make_wideband_image',
+        'imphot/scripts/regrid_hst_to_muse',
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'fix-icrs = muse_analysis.scripts.fix_icrs:main'
+    #     ],
+    # },
+)
