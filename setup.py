591d38afab44e2c2b9f9bc3ea6d4a8835262188a
Simon Conseil <contact@saimon.org>
2019-04-05 12:47:18 +0200

    Use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
-import os
 from setuptools import setup, find_packages
-
-pkgmeta = {}
-pkgmeta_file = os.path.join(os.path.dirname(__file__), 'imphot', 'version.py')
-with open(pkgmeta_file) as f:
-    code = compile(f.read(), 'version.py', 'exec')
-    exec(code, pkgmeta)
 
 setup(
     name='imphot',
-    version=pkgmeta['__version__'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
     zip_safe=False,
     install_requires=['mpdaf', 'lmfit'],
